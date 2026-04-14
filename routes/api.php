ce8efb815816a28224ef4e0f6e87462dbaac423e
Amal-Dweikat <s12323876@stu.najah.edu>
2026-04-14 23:13:41 +0300

    done

--- routes/api.php
+++ routes/api.php
@@ -11,15 +11,14 @@ use App\Http\Controllers\ItemController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/Booking', [\App\Http\Controllers\api\BookingTrip::class, 'Booking']);
-
-
-Route::middleware('auth:api')->group(function () {
-    Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/logout', [AuthController::class, 'logout']);
 Route::get('/search', [ItemController::class, 'ItemController']);
 Route::get('/test', function () {
     return "hello";
 });
 Route::middleware('auth:api')->post('/driver', [DriverController::class, 'store']);
+Route::middleware('auth:api')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 
