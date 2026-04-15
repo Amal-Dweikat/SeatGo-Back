b35499538fc7bc267fb50bbc29434a9cf5f816ca
Amal-Dweikat <s12323876@stu.najah.edu>
2026-04-15 21:53:40 +0300

    done

--- database/migrations/2026_04_13_202618_create_trips_table.php
+++ database/migrations/2026_04_13_202618_create_trips_table.php
@@ -29,7 +29,7 @@ return new class extends Migration
             $table->boolean("TripRepeat");
             $table->string('note')->default("Please be on time")->nullable();
             $table->foreignId('driver_id')->constrained()->onDelete('cascade');
-            $table->string('transport');
+            $table->string('transport')->default('bus')->nullable();
             $table->timestamps();
         });
     }
