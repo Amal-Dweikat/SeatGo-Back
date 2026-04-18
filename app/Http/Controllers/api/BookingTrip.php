32a9178af472a2103e1a1b7490b43acebd33cbf6
Fawzia-esawi <s12324291@stu.najah.edu>
2026-04-18 16:51:21 +0300

    done

--- app/Http/Controllers/api/BookingTrip.php
+++ app/Http/Controllers/api/BookingTrip.php
@@ -3,20 +3,28 @@
 namespace App\Http\Controllers\api;
 
 use App\Models\Booking;
+use App\Models\Trip;
 use Illuminate\Http\Request;
 
 class BookingTrip
 {
     public function Booking(Request $request)
     {
+
+
+
         $booking = Booking::create([
-            'trip_id' => $request->trip_id ?? 1,
-            'user_id' => auth()->id() ?? 2,
+            'trip_id' => $request->Trip_id ?? 1,
+            'user_id' => auth()->id() ,
             'numSeatBooked' => $request->NumSeat,
-            'UserWantRepeat' => $request->UserWantRepeat ?? false,
-            'EndRepeat' => $request->EndRepeat ?? null,
-            'UserSelectedDays' => $request->UserSelectedDays ?? null,
+            'UserWantRepeat' => $request->WantRepeat ?? false,
+            'EndRepeat' => $request->dateOfEndRepeat ?? null,
+            'UserSelectedDays' => $request->SelectedDays ?? null,
         ]);
+
+        $trip = Trip::find($request->Trip_id);
+        $trip->BookedSeats += $request->NumSeat;
+        $trip->save();
 
         return response()->json([
             'message' => 'Booking created successfully',
